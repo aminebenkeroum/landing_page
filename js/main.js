2a71afe48955d0e730a77b348d3e3b6157bc37e2
amine benkeroum <amine.benkeroum@gmail.com>
2016-09-08 20:00:56 +0100

    second update

--- js/main.js
+++ js/main.js
@@ -93,4 +93,71 @@ $(document).ready(function(){
   });
 
 
+  var statements = [
+    {
+      picture:"img/profile1.jpg",
+      statement : "I'm a twenty-something geek, love innovation , new technologies and have new dicussions with smart people to improve my skills",
+      color: "#1abc9c"
+    },
+    {
+      picture:"img/profile2.jpg",
+      statement : "I am a passionate traveler, and from the time I was a child, travel formed me as much as my formal education.",
+      color: "#9b59b6"
+    },
+    {
+      picture:"img/profile4.jpg",
+      statement : "Mewdo will help me meeting an open doorway with an open mind and just have a basic honest conversation",
+      color: "#e67e22"
+    },
+    {
+      picture:"img/profile3.jpg",
+      statement: "Sometimes, all I want is to talk, I don't want to be judged, I don't want to squander my time looking for an interesting conversation, I just want to be free!",
+      color:"#e74c3c"
+    },
+    {
+      picture:"img/profile5.jpg",
+      statement: "I'm looking for a running buddy around my town, I'm passionate about sports, Who want to run with me in the middle of the day or the mornings ?",
+      color:"#f39c12"
+    }
+  ]
+
+  setInterval(function(){
+
+    var statement = statements[Math.floor( statements.length * Math.random())];
+    $(".notif-left img").attr("src",statement.picture);
+    $(".notif-left p").text(statement.statement);
+    $(".notif-left").css("background-color",statement.color);
+    $(".notif-left").show();
+    $(".notif-left").removeClass("fadeOutUp");
+    $(".notif-left").addClass("fadeInUp");
+
+    setTimeout(function(){
+        $(".notif-left").removeClass("fadeInUp");
+        $(".notif-left").addClass("fadeOutUp");
+    },5000)
+
+  },6000);
+
+  setInterval(function(){
+
+    var statement = statements[Math.floor( statements.length * Math.random())];
+    $(".notif-right img").attr("src",statement.picture);
+    $(".notif-right p").text(statement.statement);
+    $(".notif-right").css("background-color",statement.color);
+    $(".notif-right").show();
+    $(".notif-right").removeClass("fadeOutUp");
+    $(".notif-right").addClass("fadeInUp");
+
+    setTimeout(function(){
+        $(".notif-right").removeClass("fadeInUp");
+        $(".notif-right").addClass("fadeOutUp");
+    },4000)
+
+  },5000);
+
+
+
+
+
+
 });
